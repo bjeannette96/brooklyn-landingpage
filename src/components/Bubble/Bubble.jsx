2713ddcc40c84d0109a677ed5109dc3e97f55afb
bjeannette96 <bachabijeannette@gmail.com>
2026-01-06 16:10:13 +0100

    Improved About section and styled buttons

--- src/components/Bubble/Bubble.jsx
+++ src/components/Bubble/Bubble.jsx
@@ -1,16 +1,16 @@
 import "./bubble.css"
 import Text from "../Text/Text";
 
-export default function Bubble() {
-    return (
-        <>               
-        <div className="bubble">
-            <a href="/home"><img src="/assets/research.png" alt="Research"/></a>
-            <Text
-                title="1. Research"
-                className="bubble-style">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla purus arcu.</p>  </Text>
+export default function Bubble({title, children, imgSrc, className=""}) {
+    return (             
+        <div className={`bubble ${className}`}>
+            <div className="icon-container">
+                <img src={imgSrc} alt={title} />
+            </div>
+            <Text title={title} className="bubble-style">
+                {children}
+            </Text>
         </div>
-        </>
     );
 }
+
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import "./button.css";
 
-export default function Button({ children, onClick, className = "" }) {
+export default function Button({ children, onClick, className = "", imgSrc }) {
   return (
     <button className={`btn ${className}`} onClick={onClick}>
       {children || "Say Hello!"}
+      {imgSrc && <img src={imgSrc} alt="" className="button-icon" />}
     </button>
   );
 }
